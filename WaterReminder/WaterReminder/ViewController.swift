b77fa143b09fb8ca4e9bbbfbeebec6bacf4f9a79
Van-Hoang-Dev <vanhoang24edu@gmail.com>
2024-04-20 13:24:19 -0700

    create project WaterReminder

---
+++ WaterReminder/WaterReminder/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  WaterReminder
+//
+//  Created by nguyenthanhnhan on 20/02/1403 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
